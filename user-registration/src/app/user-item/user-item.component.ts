80f4b85c2cd4f4cdfb09866dbf178c36000750e1
raja05082000 <167206198+raja05082000@users.noreply.github.com>
2025-07-22 19:09:02 +0530

    Update user-item.component.ts

--- user-registration/src/app/user-item/user-item.component.ts
+++ user-registration/src/app/user-item/user-item.component.ts
@@ -1,10 +1,23 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-user-item',
   templateUrl: './user-item.component.html',
-  styleUrls: ['./user-item.component.css']
+  styleUrls: ['./user-item.component.css'],
 })
 export class UserItemComponent {
-@Input() user!: { name: string, email: string, };
+  @Input() user!: { name: string; email: string };
+  @Output() deleteUser = new EventEmitter<any>();
+  @Output() editUser = new EventEmitter<any>();
+
+   openEditPopup() {
+    this.editUser.emit(this.user); 
+  }
+
+  onDelete() {
+    this.deleteUser.emit(this.user); 
+  }
+
+
+
 }
