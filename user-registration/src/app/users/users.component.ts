03cf0b1c15c31f42d2f1c8d70a3232501ff7ddb3
raja05082000 <167206198+raja05082000@users.noreply.github.com>
2025-07-22 19:04:24 +0530

    Update users.component.ts

--- user-registration/src/app/users/users.component.ts
+++ user-registration/src/app/users/users.component.ts
@@ -7,6 +7,7 @@ import { UserService } from '../user.service';
 })
 export class UsersComponent implements OnInit {
   users : any[] = [];
+   editingUser: any = null;
 
   constructor(private userService : UserService){}
 
@@ -16,5 +17,28 @@ export class UsersComponent implements OnInit {
       error: (err) => console.error(err)
     });
   }
+
+  
+  onEditUser(user: any) {
+    this.editingUser = { ...user }; 
+  }
+
+  saveEdit() {
+    const index = this.users.findIndex(u => u.id === this.editingUser.id);
+    if (index !== -1) {
+      this.users[index] = this.editingUser;
+    }
+    this.editingUser = null;
+  }
+
+  
+  cancelEdit() {
+    this.editingUser = null;
+  }
+
+  onDeleteUser(user: any) {
+    this.users = this.users.filter(u => u.id !== user.id);
+  }
+
 
 }
